9ec288cf70249f32b461c2b54360d10080feb24b
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-06-27 16:04:17 -0500

    add names to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627205503) do
+ActiveRecord::Schema.define(version: 20150627210344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20150627205503) do
     t.string   "zip"
     t.string   "county"
     t.date     "date_of_birth"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
